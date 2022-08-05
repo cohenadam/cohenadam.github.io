d0f2c7ff5d124c417596c68492a86986bc94845f
Adam Cohen <cohenadam@gmail.com>
2022-08-05 11:17:22 +0200

    working2

--- jscript.js
+++ jscript.js
@@ -2,6 +2,8 @@
 
 function showGenCode()
 {
+  document.getElementById("gencode").className = "questionbox_hide"; 
+
     // get answers and convert to integers
     let answers = [getAnswerValue("q1")];
     answers.push(getAnswerValue("q2")); 
@@ -35,7 +37,7 @@ function showGenCode()
     coderesult = coderesult.map((i) => Number(i));
 
     // create a base64 lookup table
-    let base64values = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2';
+    let base64values = 'A B C D E F G H @ J K L M N O P Q R S T U V W X Y Z a b c d e f g h ! j k ? m n o p q r s t u v w x y z 0 1 2';
     base64values = base64values.split(" ");
     let coderesultbase64 = [];
     //create base64 code
@@ -60,7 +62,7 @@ function showGenCode()
 
     // insert code result in div
         // html for the result div
-    resultHTML = '<h3>Gi denne koden til din behandler: </h3><p class = "codebox">' + coderesultbase64.join("") + "</p>";
+    resultHTML = '<h2>Gi denne koden til din behandler:<br>(Husk å skille mellom små og store bokstaver) </h2><p class = "codebox">' + coderesultbase64.join("") + "</p>" + "</p>";
     document.getElementById("generatedcode").innerHTML = resultHTML;
     // hide the submission div
     document.getElementById("questionbox10").className = "questionbox_hide";
@@ -101,13 +103,12 @@ function showPatientOrDoctor(patientordoctor) {
 // generate the result from code
 function reverseCode() {
   document.getElementById("doctorbox").className = "questionbox_hide"; 
-  document.getElementById("generatedanswers").className = "questionbox_show";
   let codeinput = document.getElementById('code').value;
   let answers = [];
   codeinput = codeinput.split("");
   
   // turn the code letter into a base64 index number
-  let base64values = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2';
+  let base64values = 'A B C D E F G H @ J K L M N O P Q R S T U V W X Y Z a b c d e f g h ! j k ? m n o p q r s t u v w x y z 0 1 2';
   base64values = base64values.split(" ");
   for(a = 0; a < codeinput.length; a++) {
     codeinput[a] = base64values.indexOf(codeinput[a]);
@@ -131,8 +132,45 @@ let nextanswer;
     }
 
     coderesult = coderesult.map((i) => Number(i));
+    const sumresult = coderesult.reduce((partialSum, a) => partialSum + a, 0);
+    coderesulttext = '<h2  style="text-align:left;">1. Stemningsleie: ' + coderesult[0] + '<br>' + 
+    '2. Følelse av indre uro / spenning: ' + coderesult[1] +  '<br>' + 
+    '3. Søvn: ' + coderesult[2] +  '<br>' + 
+    '4. Matlyst: ' + coderesult[3] +  '<br>' + 
+    '5. Konsentrasjonsevne: ' + coderesult[4] +  '<br>' + 
+    '6. Initiativ: ' + coderesult[5] +  '<br>' + 
+    '7. Følelsesmessig engasjement: ' + coderesult[6] +  '<br>' + 
+    '8. Pessimisme: ' + coderesult[7] +  '<br>' + 
+    '9. Livslyst: ' + coderesult[8] +  '<br><br>' + 
+    'Total skår: ' + sumresult;
 
-    document.getElementById("generatedanswers").className = "questionbox_show";
-    document.getElementById("generatedanswers").innerHTML = coderesult;
+    document.getElementById("generatedanswers").innerHTML = coderesulttext;
+    document.getElementById("questionbox10").className = "questionbox_show"; 
+
+    return coderesult;
+}
+
+function showFinalResult() {
+  document.getElementById("questionbox10").className = "questionbox_hide"; 
+  coderesult = reverseCode();
+  coderesult.push(getAnswerValue("q10")); 
+  coderesult = coderesult.map((i) => Number(i));
+
+  const sumresult = coderesult.reduce((partialSum, a) => partialSum + a, 0);
+  coderesulttext = '<h2  style="text-align:left;">1. Stemningsleie: ' + coderesult[0] + '<br>' + 
+  '2. Følelse av indre uro / spenning: ' + coderesult[1] +  '<br>' + 
+  '3. Søvn: ' + coderesult[2] +  '<br>' + 
+  '4. Matlyst: ' + coderesult[3] +  '<br>' + 
+  '5. Konsentrasjonsevne: ' + coderesult[4] +  '<br>' + 
+  '6. Initiativ: ' + coderesult[5] +  '<br>' + 
+  '7. Følelsesmessig engasjement: ' + coderesult[6] +  '<br>' + 
+  '8. Pessimisme: ' + coderesult[7] +  '<br>' + 
+  '9. Livslyst: ' + coderesult[8] +  '<br>' +
+  '10. Synlig tristhet – legeobservert:' + coderesult[9] + '<br><br>' + 
+  'Total skår: ' + sumresult +  '<br><br>' + 
+  '<ul><li>12–20: Pasienten kan ha nytte av antidepressive midler i tillegg til samtaler</li><li>≥20: Mange vil ha nytte av medikamentell antidepressiv behandling, såfremt depresjonen har vart i minst 2 uker og ikke skyldes sorg.</li><li>≥35: Vurder innleggelse, eventuelt henvisning til DPS (distriktspsykiatrisk senter) eller spesialist.<br>Suicidalitetsskår ≥4: Vurder innleggelse uavhengig av sumskår.</li><ul>';
+  document.getElementById("generatedanswers").innerHTML = coderesulttext;
+  document.getElementById("generatedanswers").className = "questionbox_show";
+  document.getElementById("questionbox10").className = "questionbox_hide"; 
 
 }
